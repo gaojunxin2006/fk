021984220a216fba5933d103dd5980752064d0d0
wii <wii@wiideMacBook-Pro.local>
2021-06-17 17:15:41 +0800

    Initial commit

---
+++ src/main/java/fk/FkApplication.java
@@ -0,0 +1,22 @@
+package fk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FkApplication.class, args);
+
+        System.out.println("srpintbooot启动成功！");
+
+
+    }
+
+
+
+
+
+
+}
